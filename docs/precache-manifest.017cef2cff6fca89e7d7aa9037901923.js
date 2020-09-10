fb38ed99215468eb1c550bb3632dff0b7fb8b489
safinsingh <safin.singh@gmail.com>
2020-09-10 13:12:14 -0700

    why am i doing this? oh idk

---
+++ docs/precache-manifest.017cef2cff6fca89e7d7aa9037901923.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f73be58cf35f0f156988b15e02a038",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb740e60a1522d3ff670",
+    "url": "/static/js/2.0d232276.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d232276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "518326d4e46c794b3caf",
+    "url": "/static/js/main.814be473.chunk.js"
+  },
+  {
+    "revision": "738d01ce0547e2492752",
+    "url": "/static/js/runtime-main.37c6d9ca.js"
+  }
+]);
