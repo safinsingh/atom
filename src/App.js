b4435f3d8a06255040bd3ca2676ee470ab0d1c1a
safinsingh <safin.singh@gmail.com>
2020-09-10 13:10:34 -0700

    make it gooder but still some bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import * as _ from "underscore";
 
 class App extends React.Component {
 	state = {
@@ -10,12 +11,10 @@ class App extends React.Component {
 			...this.state,
 			elements: {
 				...this.state.elements,
-				[Object.keys(this.state.elements).length + 1]: [
-					{
-						mass: "0",
-						percentage: "0",
-					},
-				],
+				[Object.keys(this.state.elements).length + 1]: {
+					mass: "0",
+					percentage: "0",
+				},
 			},
 		});
 	}
@@ -46,46 +45,71 @@ class App extends React.Component {
 		}
 	}
 
+	delete(e) {
+		const newState = {
+			elements: _.omit(this.state.elements, e.target.id),
+		};
+		this.setState(newState);
+	}
+
 	render() {
+		const { elements } = this.state;
 		return (
 			<div>
 				<h1>get the atomic mass of an arbitrary atom</h1>
+				<p>you copy paste the following</p>
 				<h1>
-					{Object.keys(this.state.elements).length !== 0 &&
-						Object.keys(this.state.elements)
+					{Object.keys(elements).map((key, index) => (
+						<>
+							{elements[key].mass}*{elements[key].percentage}
+							{Object.keys(elements).length >= 2 &&
+								index + 1 !== Object.keys(elements).length && (
+									<> + </>
+								)}
+						</>
+					))}{" "}
+					={" "}
+					{Object.keys(elements).length !== 0 &&
+						Object.keys(elements)
 							.reduce(
 								(a, cv) =>
 									a +
-									parseFloat(
-										this.state.elements[`${cv}`].mass
-									) *
+									parseFloat(elements[`${cv}`].mass) *
 										parseFloat(
-											this.state.elements[`${cv}`]
-												.percentage
+											elements[`${cv}`].percentage
 										) *
 										0.01,
 								0
 							)
-							.toFixed(3)}
+							.toFixed(2)}{" "}
+					amu
 				</h1>
-				<button onClick={this.newElement.bind(this)}>
+				<button
+					onClick={this.newElement.bind(this)}
+					style={{ margin: "5px" }}
+				>
 					new isotope
 				</button>
 				<br />
-				{Object.keys(this.state.elements).map((e) => (
+				{Object.keys(elements).map((e) => (
 					<>
 						<input
-							value={this.state.elements[`${e}`].mass}
+							value={elements[`${e}`].mass}
 							placeholder='mass'
 							id={e}
 							onChange={this.onChange.bind(this)}
+							style={{ margin: "5px" }}
 						/>
 						<input
-							value={this.state.elements[`${e}`].percentage}
+							value={elements[`${e}`].percentage}
 							placeholder='percentage'
 							id={e}
 							onChange={this.onChange.bind(this)}
+							style={{ margin: "5px", marginBottom: "5px" }}
 						/>
+						<button id={e} onClick={this.delete.bind(this)}>
+							x
+						</button>
 						<br />
 					</>
 				))}
